4827fd21a7e6c2260f1b73c8519aa8455f2a81bc
Phaneesh28 <95366988+Phaneesh28@users.noreply.github.com>
2024-12-06 03:15:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const products = require('./models/products');
 const userData = require('./models/users');
 
 const app = express();
+app.set('trust proxy', 1);
 // const favicon = require('serve-favicon');
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
